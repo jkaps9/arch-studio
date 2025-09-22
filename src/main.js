be6cde68c7d474fa6998e925c852a2d3a72582bd
Josh <15219491+jkaps9@users.noreply.github.com>
2025-09-22 15:53:49 -0400

    add click listener to slide switcher

--- src/main.js
+++ src/main.js
@@ -1,8 +1,41 @@
 import "./style.css";
 
-const navToggle = document.querySelector(".nav-toggle");
-const nav = document.querySelector(".nav");
+addNavToggleOnClick();
 
-navToggle.addEventListener("click", () => {
-  nav.classList.toggle("visible");
-});
+function addNavToggleOnClick() {
+  const navToggle = document.querySelector(".nav-toggle");
+  const nav = document.querySelector(".nav");
+
+  navToggle.addEventListener("click", () => {
+    nav.classList.toggle("visible");
+  });
+}
+
+removeActiveFromSlideSwitcher();
+
+function removeActiveFromSlideSwitcher() {
+  const slideSwitcher = document.querySelector(".slide-switcher");
+  const slideIndicators = slideSwitcher.querySelectorAll(".slide-indicator");
+
+  slideIndicators.forEach((element) => {
+    element.classList.remove("active");
+  });
+}
+
+addSlideIndicatorOnClick();
+
+function addSlideIndicatorOnClick() {
+  const slideIDs = ["slide-one", "slide-two", "slide-three", "slide-four"];
+  const slideSwitcher = document.querySelector(".slide-switcher");
+  const slideIndicators = slideSwitcher.querySelectorAll(".slide-indicator");
+  slideIndicators.forEach((element) => {
+    element.addEventListener("click", (slideIndicator) => {
+      console.log(slideIndicator);
+      switchSlide(slideIDs.indexOf(slideIndicator.target.id));
+    });
+  });
+}
+
+function switchSlide(slideNumber) {
+  console.log(slideNumber);
+}
