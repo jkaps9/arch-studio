1764c4af05021f4dc0224018e02c4744b10a8901
Josh <15219491+jkaps9@users.noreply.github.com>
2025-09-22 16:01:04 -0400

    update slide text on click, remove p tag from btn

--- src/main.js
+++ src/main.js
@@ -28,7 +28,6 @@ function addSlideIndicatorOnClick() {
   const slideIndicators = slideSwitcher.querySelectorAll(".slide-indicator");
   slideIndicators.forEach((element) => {
     element.addEventListener("click", (slideIndicator) => {
-      console.log(slideIndicator);
       switchSlide(slideIDs.indexOf(slideIndicator.target.id));
     });
   });
@@ -36,4 +35,44 @@ function addSlideIndicatorOnClick() {
 
 function switchSlide(slideNumber) {
   console.log(slideNumber);
+  const slides = [
+    {
+      title: "Project Paramour",
+      description:
+        "Project made for an art museum near Southwest London. Project Paramour is a statement of bold, modern architecture.",
+      mobileImg: "x",
+      tabletImg: "x",
+      desktopImg: "x",
+    },
+    {
+      title: "Seraph Station",
+      description:
+        "The Seraph Station project challenged us to design a unique station that would transport people through time. The result is a fresh and futuristic model inspired by space stations.",
+      mobileImg: "x",
+      tabletImg: "x",
+      desktopImg: "x",
+    },
+    {
+      title: "Federal II Tower",
+      description:
+        "A sequel theme project for a tower originally built in the 1800s. We achieved this with a striking look of brutal minimalism with modern touches.",
+      mobileImg: "x",
+      tabletImg: "x",
+      desktopImg: "x",
+    },
+    {
+      title: "Trinity Bank Tower",
+      description:
+        "Trinity Bank challenged us to make a concept for a 84 story building located in the middle of a city with a high earthquake frequency. For this project we used curves to blend design and stability to meet our objectives.",
+      mobileImg: "x",
+      tabletImg: "x",
+      desktopImg: "x",
+    },
+  ];
+
+  const projectTitle = document.querySelector(".hero .container h1");
+  const projectDescription = document.querySelector(".hero .container p");
+
+  projectTitle.textContent = slides[slideNumber].title;
+  projectDescription.textContent = slides[slideNumber].description;
 }
