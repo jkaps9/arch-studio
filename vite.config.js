c732c2cb70146e0866063a6d654a3211a1b2912a
Josh <15219491+jkaps9@users.noreply.github.com>
2025-09-23 23:25:48 -0400

    fix links in build version

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,14 @@
 import { defineConfig } from "vite";
 
-export default defineConfig({});
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: "./index.html",
+        portfolio: "./src/pages/portfolio.html",
+        about: "./src/pages/about.html",
+        contact: "./src/pages/contact.html",
+      },
+    },
+  },
+});
