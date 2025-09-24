96c0d82b186f18f613cab3e6a2fa4a8408c956d4
Josh <15219491+jkaps9@users.noreply.github.com>
2025-09-23 23:39:35 -0400

    remove unneeded scripts

--- src/scripts/about.js
+++
@@ -1,5 +0,0 @@
-import "../../src/style.css";
-import "../styles/about.css";
-import { initializeNav } from "../../src/scripts/nav.js";
-
-initializeNav();
--- src/scripts/contact.js
+++
@@ -1,7 +0,0 @@
-import "../../src/style.css";
-import "../styles/contact.css";
-import "../styles/about.css";
-
-import { initializeNav } from "../../src/scripts/nav.js";
-
-initializeNav();
